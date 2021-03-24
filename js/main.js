695af37a3c136b47198673887e7e803a5fdb3d51
Elvina Pavlovskaya <pavl.elvina@gmail.com>
2021-03-24 23:02:02 +0300

    release

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+});
